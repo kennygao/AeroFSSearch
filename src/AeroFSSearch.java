938b61665eb78cc35f9ba0ec81e3b4bacb0feb71
kg <kennytgao@gmail.com>
2014-04-01 20:05:56 -0700

    laid out basic structure

--- src/AeroFSSearch.java
+++ src/AeroFSSearch.java
@@ -2,12 +2,32 @@
  * AeroFS Challenge - Kenny Gao
  */
 
-import java.util.stream.IntStream;
-
 /**
  * @author kennygao
  */
 public class AeroFSSearch {
+
+    public enum SearchType {
+        LESS_THAN, LESS_THAN_EQUALS, EQUALS, GREATER_THAN_EQUALS, GREATER_THAN
+    }
+
+    public enum ResultType {
+        NOT_FOUND, FOUND_LESS, FOUND_EXACT, FOUND_GREATER
+    }
+
+    public class SearchResult {
+        ResultType type;
+        int index;
+
+        public SearchResult(ResultType type, int index) {
+            this.type = type;
+            this.index = index;
+        }
+    }
+
+    public static SearchResult search(int[] items, int n_items, boolean ascending, int key, SearchType type) {
+        return null;
+    }
 
     /**
      * @param args unused
