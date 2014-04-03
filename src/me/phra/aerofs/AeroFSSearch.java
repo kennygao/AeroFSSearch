0726a62bb8b9218379071351a3f6692518b75538
kg <kennytgao@gmail.com>
2014-04-03 16:49:02 -0700

    implemented search

--- src/me/phra/aerofs/AeroFSSearch.java
+++ src/me/phra/aerofs/AeroFSSearch.java
@@ -4,6 +4,11 @@
 
 package me.phra.aerofs;
 
+import java.util.OptionalInt;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
 /**
  * @author kennygao
  */
@@ -27,15 +32,55 @@ public class AeroFSSearch {
         }
     }
 
-    public static SearchResult search(int[] items, int n_items, boolean ascending, int key, SearchType type) {
-        return null;
-    }
+    public static SearchResult search(int[] items, int n_items, boolean ascending, int key, SearchType searchType) {
+        IntPredicate predicate = null;
+        IntBinaryOperator operator = null;
 
-    /**
-     * @param args unused
-     */
-    public static void main(String[] args) {
-        System.out.println();
+        final IntBinaryOperator maxOperator = (a, b) -> items[a] > items[b] ? a : b;
+        final IntBinaryOperator nullOperator = (a, b) -> -1; // will never be used
+        final IntBinaryOperator minOperator = (a, b) -> items[a] < items[b] ? a : b;
+
+        switch (searchType) {
+            case LESS_THAN:
+                predicate = i -> items[i] < key;
+                operator = maxOperator;
+                break;
+            case LESS_THAN_EQUALS:
+                predicate = i -> items[i] <= key;
+                operator = maxOperator;
+                break;
+            case EQUALS:
+                predicate = i -> items[i] == key;
+                operator = nullOperator;
+                break;
+            case GREATER_THAN_EQUALS:
+                predicate = i -> items[i] >= key;
+                operator = minOperator;
+                break;
+            case GREATER_THAN:
+                predicate = i -> items[i] > key;
+                operator = minOperator;
+                break;
+        }
+
+        OptionalInt candidate = IntStream.range(0, n_items).filter(predicate).reduce(operator);
+
+        if (!candidate.isPresent()) {
+            return new SearchResult(ResultType.NOT_FOUND, -1);
+        }
+
+        int index = candidate.getAsInt();
+
+        switch (Integer.signum(Integer.compare(items[index], key))) {
+            case -1:
+                return new SearchResult(ResultType.FOUND_LESS, index);
+            case 0:
+                return new SearchResult(ResultType.FOUND_EXACT, index);
+            case 1:
+                return new SearchResult(ResultType.FOUND_GREATER, index);
+        }
+
+        return null;
     }
 
 }
